9fdfb0ad53a28bb4ba854e533560956bcc61dfda
Jonathan Linnestad <jonathan.s.linnestad@gmail.com>
2015-11-09 19:18:04 +0100

    add text

--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -17,7 +17,7 @@ SignUp = React.createClass({
 			<div>
 	   		<h2>Sign Up</h2>
 		    	<form className="register" onSubmit={this.handleSubmit}>
-			        <p>Email: <input type="email" name="email" ref="email"/>*Do not use your real email address, make something up</p>
+			        <p>Email: <input type="email" name="email" ref="email"/>*Do not use your real email address,sdf make something up</p>
 			        <p>Password: <input type="password" name="password" ref="password"/>*Do not use one of your usual password, make something up</p>
 			        <p><input type="submit" value="Sign Up"/></p>
 		    	</form>
@@ -26,3 +26,5 @@ SignUp = React.createClass({
 	}
 
 })
+
+
